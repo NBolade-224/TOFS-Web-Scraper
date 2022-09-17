bb56c49ff4e8c8d4589b619cb3403034707005b5
NBolade-224 <Nickbolade1@gmail.com>
2022-09-17 18:46:15 +0100

    Notes

--- TOFS Web Scaper.py
+++ TOFS Web Scaper.py
@@ -1,11 +1,15 @@
 from requests_html import AsyncHTMLSession
-
 import openpyxl 
 
+#########################################################################################################################################
+################ EXCEL FILE: THIS FILE WILL PROVIDE THE SKUS THAT WILL BE SEARCHED, AND THEN THE PRICES FOUND WILL BE WRITTEN TO THIS FILE.
+#########################################################################################################################################
 ExcelFile = openpyxl.load_workbook("C:\\Users\\nickb\\Videos\\Commercial Testing\\MHStar SKU.xlsx")
 ExcelSheet = ExcelFile.active
+
 mylist = []
 dict2 = {}
+
 for col in ExcelSheet['A']:
      mylist.append(col.value)
      dict2[col.value] = "N/A"
@@ -24,6 +28,8 @@ urls1 = [
 "https://www.aosom.co.uk/patio-lawn-garden/garden-tools-c800.html",
 "https://www.aosom.co.uk/patio-lawn-garden/barbecues-c748.html",]
 
+######## Two URL groups are used to prevent overload on requests to the website
+
 urls2 = [
 "https://www.aosom.co.uk/patio-lawn-garden/garden-decor-c801.html",
 "https://www.aosom.co.uk/patio-lawn-garden/garden-planters-stands-c799.html",
@@ -37,6 +43,11 @@ urls2 = [
 "https://www.aosom.co.uk/category/storage-cleaning-solutions~425/",
 "https://www.aosom.co.uk/category/shoe-bench~626/",
 "https://www.aosom.co.uk/category/bathroom~421/",]
+
+#########################################################################################################################################
+################ THIS FUNCTION RUNS ASYNC AND WILL LOOP THROUGH EACH CATEGORY OF PRODUCTS UNTILL A MATCH IS FOUND (FROM SKUS LISTED IN EXCEL FILE)
+################ WHEN A MATCH IS FOUND, THE SKU PRODUCT AND ITS PRICE ARE HELD IN A DICTIONARY TO BE LATER WRITTEN TO EXCEL
+#########################################################################################################################################
 
 async def Scraper1(vurl):
     m = 1
@@ -55,16 +66,18 @@ async def Scraper1(vurl):
                 print()
         m += 1
 
-
 asession.run(*[lambda vurl=vurl: Scraper1(vurl) for vurl in urls1])
 asession.run(*[lambda vurl=vurl: Scraper1(vurl) for vurl in urls2])
+
+#########################################################################################################################################
+################ THIS FINAL PART WRITES ALL THE DATA FROM THE TEMP DICTIONARY, INTO THE EXCEL FILE FOR COMMERCIAL ANALYSIS
+#########################################################################################################################################
 
 for x in dict2:
     Row = mylist.index(x) + 1
     ExcelSheet['B%d' % int(Row)] = dict2.get(x)
 
 ExcelSheet['B1'] = "Price"
-
 
 ExcelFile.save('C:\\Users\\nickb\\Videos\\Commercial Testing\\MHStar SKU.xlsx')
 
